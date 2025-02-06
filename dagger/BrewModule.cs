886408b85ad10f420a24ad51e31a1aabba1a0ce5
Patrick Magee <patrick.magee@live.co.uk>
2025-02-06 21:06:57 +0000

    dagger dotnet sdk + dagger gha module
    Signed-off-by: Patrick Magee <patrick.magee@live.co.uk>

---
+++ dagger/BrewModule.cs
@@ -0,0 +1,55 @@
+using System.Text.Json.Serialization;
+using Dagger;
+using static Dagger.Alias;
+
+public class BrewModule : IJsonOnDeserialized
+{
+	/// <summary>
+	/// A container that will build the solution in the given directory.
+	/// </summary>
+	public static Container Build([DirectoryFromContext(DefaultPath = ".")] Directory directoryArg)
+	{
+		var cache = DAG.CacheVolume(key: "nuget");
+		
+		return DAG
+			.Container()
+			.From("mcr.microsoft.com/dotnet/sdk:8.0")
+			.WithMountedCache("/root/.nuget/packages", cache)
+			.WithMountedDirectory("/mnt", directoryArg)
+			.WithWorkdir("/mnt")
+			.WithExec(["dotnet", "build", "Brew.sln", "-c", "Release", "-o", "/app"]);
+	}
+
+	/// <summary>
+	/// run the built solution in the given directory.
+	/// </summary>
+	public static Container Run([DirectoryFromContext(DefaultPath = ".")] Directory directoryArg)
+	{
+		var build = Build(directoryArg);
+		
+		return DAG.Container()
+			.From("mcr.microsoft.com/dotnet/runtime:8.0")
+			.WithMountedDirectory("/app", build.Directory("/app"))
+			.WithWorkdir("/app")
+			.WithExec(["./Brew.Console"]);
+	}
+
+	/// <summary>
+	/// dagger call ci export --path .
+	/// </summary>
+	public static Directory Ci()
+	{
+		return DAG.Gha().WithWorkflow(
+			DAG.Gha().Workflow("build and run features").WithJob(DAG.Gha().Job(
+					name: "build and run features",
+					command: "run"
+				)
+			)
+		).Generate();
+	}
+
+	void IJsonOnDeserialized.OnDeserialized()
+	{
+		
+	}
+}
