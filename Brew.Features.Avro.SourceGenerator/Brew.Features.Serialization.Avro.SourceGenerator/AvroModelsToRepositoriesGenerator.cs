38d4db02e4ebe3aecd0310d4551270f129491271
Patrick Magee <patrick.magee@live.co.uk>
2025-11-10 22:23:03 +0000

    Upgrade to .NET 10 and enhance CLI with multiple execution modes
    - Migrated all projects from .NET 9 to .NET 10
    - Updated Dagger module to TypeScript with .NET 10 support
    - Enhanced Brew.Console with list/run/all command modes
    - Fixed BackgroundService cancellation in QueueWriter, QueueProcessor, PipeServer, PipeClient
    - Updated to modern .NET APIs (ReadExactlyAsync)
    - Generated three separate GHA workflows: list-brews, run-all-brews, run-one-brew
    - Removed old C# Dagger module and .sln file
    - Added Directory.Packages.props for centralized package management

--- Brew.Features.Avro.SourceGenerator/Brew.Features.Serialization.Avro.SourceGenerator/AvroModelsToRepositoriesGenerator.cs
+++ Brew.Features.Avro.SourceGenerator/Brew.Features.Serialization.Avro.SourceGenerator/AvroModelsToRepositoriesGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.CodeAnalysis;
@@ -8,63 +9,64 @@ using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 namespace Brew.Features.Serialization.Avro.SourceGenerator;
 
 [Generator]
-public class AvroModelsToRepositoriesGenerator : ISourceGenerator
+public sealed class AvroModelsToRepositoriesGenerator : IIncrementalGenerator
 {
-    public void Initialize(GeneratorInitializationContext context)
+    public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        
-    }
+        var classDecls = context.SyntaxProvider.CreateSyntaxProvider(
+            static (node, _) => node is ClassDeclarationSyntax cls && cls.BaseList is not null,
+            static (ctx, _) => (ClassDeclarationSyntax)ctx.Node);
 
-    public void Execute(GeneratorExecutionContext context)
-    {
-        var modelRepositories = new List<MemberDeclarationSyntax>();
-
-        foreach (var tree in context.Compilation.SyntaxTrees)
-        {
-            var classes = tree.GetRoot().DescendantNodes().OfType<ClassDeclarationSyntax>();
-            var avroModels = classes.Where(x => x.BaseList != null && x.BaseList.DescendantTokens().Any(t => t.Text.Equals("ISpecificRecord")));
-
-            foreach (var @class in avroModels)
+        var avroModelNames = classDecls
+            .Combine(context.CompilationProvider)
+            .Select(static (pair, _) =>
             {
-                modelRepositories.Add(FieldDeclaration(
-                    VariableDeclaration(
-                            GenericName(
-                                    Identifier("GenericRepository"))
-                                .WithTypeArgumentList(
-                                    TypeArgumentList(
-                                        SingletonSeparatedList<TypeSyntax>(
-                                            IdentifierName(@class.Identifier.Text)))))
-                        .WithVariables(
-                            SingletonSeparatedList(
-                                VariableDeclarator(
-                                        Identifier(@class.Identifier.Text + "Repository"))
-                                    .WithInitializer(
-                                        EqualsValueClause(
-                                            ImplicitObjectCreationExpression()))))));
+                var (cls, compilation) = pair;
+                var model = compilation.GetSemanticModel(cls.SyntaxTree);
+                var symbol = model.GetDeclaredSymbol(cls) as INamedTypeSymbol;
+                if (symbol is null)
+                    return null;
+                var implements = symbol.AllInterfaces.Any(i => string.Equals(i.Name, "ISpecificRecord", StringComparison.Ordinal));
+                return implements ? symbol.Name : null;
+            })
+            .Where(static name => name is not null)!
+            .Select(static (name, _) => name!);
+
+        context.RegisterSourceOutput(avroModelNames.Collect(), static (ctx, names) =>
+        {
+            var unique = names.Distinct().OrderBy(n => n).ToList();
+            var modelRepositories = new List<MemberDeclarationSyntax>();
+            foreach (var n in unique)
+            {
+                modelRepositories.Add(
+                    FieldDeclaration(
+                        VariableDeclaration(
+                                GenericName(Identifier("GenericRepository"))
+                                    .WithTypeArgumentList(
+                                        TypeArgumentList(
+                                            SingletonSeparatedList<TypeSyntax>(IdentifierName(n)))))
+                            .WithVariables(
+                                SingletonSeparatedList(
+                                    VariableDeclarator(Identifier(n + "Repository"))
+                                        .WithInitializer(EqualsValueClause(ImplicitObjectCreationExpression()))))));
             }
-        }
 
-        var code = CompilationUnit()
-            .WithMembers(
-                SingletonList<MemberDeclarationSyntax>(
-                    FileScopedNamespaceDeclaration(
-                            QualifiedName(
-                                IdentifierName("Brew"),
-                                IdentifierName("Avro")))
-                        .WithUsings(
-                            SingletonList(
-                                UsingDirective(
-                                    QualifiedName(
-                                        IdentifierName("Brew"),
-                                        IdentifierName("Models")))))
+            var code = CompilationUnit()
+                .WithMembers(
+                    SingletonList<MemberDeclarationSyntax>(
+                        FileScopedNamespaceDeclaration(
+                                QualifiedName(IdentifierName("Brew"), IdentifierName("Avro")))
+                            .WithUsings(
+                                SingletonList(
+                                    UsingDirective(QualifiedName(IdentifierName("Brew"), IdentifierName("Models")))))
+                            .WithMembers(
+                                SingletonList<MemberDeclarationSyntax>(
+                                    ClassDeclaration("Repositories")
+                                        .WithModifiers(TokenList(Token(SyntaxKind.PublicKeyword)))
+                                        .WithMembers(List(modelRepositories))))))
+                .NormalizeWhitespace();
 
-                        .WithMembers(
-                            SingletonList<MemberDeclarationSyntax>(
-                                ClassDeclaration("Repositories")
-                                    .WithModifiers(TokenList(Token(SyntaxKind.PublicKeyword)))
-                                    .WithMembers(List(modelRepositories))))));
-
-
-        context.AddSource("ModelsApi.g.cs", code.NormalizeWhitespace().ToFullString());
+            ctx.AddSource("ModelsApi.g.cs", code.ToFullString());
+        });
     }
 }
--- Brew.Features.IO.Channels.MessageBus/QueueWriter.cs
+++ Brew.Features.IO.Channels.MessageBus/QueueWriter.cs
@@ -7,10 +7,24 @@ internal sealed class QueueWriter(IEventBus bus, ILogger<QueueProcessor> logger)
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        try
         {
-            await bus.Publish(new HelloWorldEvent(), stoppingToken);
-            await Task.Delay(1000, stoppingToken);
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    await bus.Publish(new HelloWorldEvent(), stoppingToken);
+                    await Task.Delay(1000, stoppingToken);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Error publishing event");
+                }
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            logger.LogInformation("QueueWriter has been cancelled");
         }
     }
 }
